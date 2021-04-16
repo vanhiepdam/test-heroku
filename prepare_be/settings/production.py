c7308a782166b971c21d48efc30bf9e9cd47bd17
Dam Hiep <hiep@kiuglobal.com>
2021-04-16 14:10:04 +0700

    update production setting

--- prepare_be/settings/production.py
+++ prepare_be/settings/production.py
@@ -2,3 +2,13 @@
 from .base import *
 
 CORS_ALLOW_ALL_ORIGINS = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hiep-todo',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+    }
+}
